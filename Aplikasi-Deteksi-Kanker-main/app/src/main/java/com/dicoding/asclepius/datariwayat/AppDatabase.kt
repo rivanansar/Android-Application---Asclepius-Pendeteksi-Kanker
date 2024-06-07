49f2755748012385504f7364fac8ae67e8f5d1e2
rivanansar <122036556+rivanansar@users.noreply.github.com>
2024-06-07 19:08:00 +0700

    Add files via upload

---
+++ Aplikasi-Deteksi-Kanker-main/app/src/main/java/com/dicoding/asclepius/datariwayat/AppDatabase.kt
@@ -0,0 +1,37 @@
+package com.dicoding.asclepius.datariwayat
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [PredictionHistory::class], version = 2, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun predictionHistoryDao(): PredictionHistoryDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                )
+//                    .addMigrations(MIGRATION_1_2)
+                    .build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+
+    }
+
+}
